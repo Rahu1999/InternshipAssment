c49800272bfefc473b514b782f81d7303079db79
unknown <rahulrajbhar19999@gmail.com>
2021-03-18 12:08:23 +0530

    first commit

---
+++ Java/abcdtPartten.java
@@ -0,0 +1,16 @@
+public class abcdtPartten {
+    public static void main(String args[]) {
+        int i, j,n=5;
+        int alphabet = 65;
+        for(i=0; i<n; i++) {
+            for(j=2*(n-i); j>=0; j--){
+                System.out.print(" ");
+            }
+            for(j=0; j<=i; j++){
+                System.out.print((char) alphabet+" ");
+                alphabet++;
+            }
+            System.out.println();
+        }
+    }
+}
